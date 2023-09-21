67f2df563acfcf91c11d551003d94421522acd20
teman67 <>
2023-09-21 09:05:43 +0000

    add questions to the questions file

---
+++ javascript/questions.js
@@ -0,0 +1,123 @@
+const questions = [
+    {
+        question: "What does the prefix 'nano' in nanotechnology refer to?",
+        image: "question1.jpg",
+        options: [
+            "A type of computer",
+            "A very small size",
+            "A unit of time",
+            "A type of food",
+            "A type of car"
+        ],
+        answer: 1 // Index of the correct option (in this case, "A very small size")
+    },
+    {
+        question: "Which of the following is a common use of nanotechnology in sunscreen?",
+        image: "question2.jpg",
+        options: [
+            "Making it taste better",
+            "Improving internet speed",
+            "Increasing UV protection",
+            "Enhancing smartphone battery life",
+            "Enhancing the flavor of food"
+        ],
+        answer: 2 // Index of the correct option (in this case, "Increasing UV protection")
+    },
+    {
+        question: "What is the size range typically associated with nanoscale materials?",
+        image: "question3.jpg",
+        options: [
+            "1 meter to 10 meters",
+            "1 millimeter to 1 centimeter",
+            "1 nanometer to 100 nanometers",
+            "1 micrometer to 1 millimeter",
+            "1 picometer to 1 nanometer"
+        ],
+        answer: 2 // Index of the correct option (in this case, "1 nanometer to 100 nanometers")
+    },
+    {
+        question: "Which field of science and technology is nanotechnology most closely related to?",
+        image: "question4.jpg",
+        options: [
+            "Astronomy",
+            "Biology",
+            "Geology",
+            "Physics",
+            "Chemistry"
+        ],
+        answer: 5 // Index of the correct option (in this case, "Chemistry")
+    },
+    {
+        question: "What is the primary benefit of using nanomaterials in manufacturing?",
+        image: "question5.jpg",
+        options: [
+            "Reduced cost",
+            "Increased size",
+            "Improved taste",
+            "Enhanced strength",
+            "Faster production speed"
+        ],
+        answer: 3 // Index of the correct option (in this case, "Enhanced strength")
+    },
+    {
+        question: "Which of the following is NOT a potential application of nanotechnology?",
+        image: "question6.jpg",
+        options: [
+            "Drug delivery",
+            "Water purification",
+            "Space travel",
+            "Energy storage",
+            "Building larger bridges"
+        ],
+        answer: 4 // Index of the correct option (in this case, "Building larger bridges")
+    },
+    {
+        question: "What is the term used to describe materials that exhibit unique properties at the nanoscale?",
+        image: "question7.jpg",
+        options: [
+            "Macroscopic materials",
+            "Nanoscale materials",
+            "Quantum materials",
+            "Superconductors",
+            "Nanomaterials"
+        ],
+        answer: 5 // Index of the correct option (in this case, "Nanomaterials")
+    },
+    {
+        question: "Which scientist is often credited with popularizing the concept of nanotechnology?",
+        image: "question8.jpg",
+        options: [
+            "Isaac Newton",
+            "Albert Einstein",
+            "Richard Feynman",
+            "Marie Curie",
+            "Nikola Tesla"
+        ],
+        answer: 3 // Index of the correct option (in this case, "Richard Feynman")
+    },
+    {
+        question: "What is the primary purpose of a scanning tunneling microscope (STM) in nanotechnology?",
+        image: "question9.jpg",
+        options: [
+            "To cut nanoscale materials",
+            "To measure electrical resistance",
+            "To create nanoscale artwork",
+            "To image and manipulate individual atoms",
+            "To generate electricity"
+        ],
+        answer: 4 // Index of the correct option (in this case, "To image and manipulate individual atoms")
+    },
+    {
+        question: "In nanotechnology, what does 'self-assembly' refer to?",
+        image: "question10.jpg",
+        options: [
+            "Creating robots that build structures",
+            "Materials organizing themselves into ordered structures",
+            "Building small houses",
+            "Developing self-cooking appliances",
+            "Creating self-driving cars"
+        ],
+        answer: 2 // Index of the correct option (in this case, "Materials organizing themselves into ordered structures")
+    },
+    // Add more questions here...
+];
